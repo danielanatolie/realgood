c05fe488e3789200752cd46d2bb9e6d9ea3c39d1
Daniel Anatolie <daniel.anatolie@gmail.com>
2018-09-22 13:11:33 -0700

    Updated sample requests

--- backend/server.js
+++ backend/server.js
@@ -2,20 +2,50 @@
   All of these functions are going to be living in the aws cloud as lambdas
 */
 // Sample POST request to APIGateway -> postResource Lambda
-var xhr = new XMLHttpRequest();
-xhr.open('POST', 'https://5tg521dz44.execute-api.us-east-2.amazonaws.com/dev/realgood');
-xhr.onreadystatechange = function(event) {
-  console.log(event.target.response);
+function postResource() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', 'https://5tg521dz44.execute-api.us-east-2.amazonaws.com/dev/realgood');
+  xhr.onreadystatechange = function(event) {
+    console.log(event.target.response);
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send(JSON.stringify({
+    ResourceId: "test_2",
+    date: "Sat. Sep. 22, 2018",
+    location: "UBC Hospital",
+    name: "Health Check",
+    type: "healthcare"
+  }));
 }
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-  ResourceId: "test_2",
-  date: "Sat. Sep. 22, 2018",
-  location: "UBC Hospital",
-  name: "Health Check",
-  type: "healthcare"
-}));
-
-// Sample GET request to APIGateway -> getResource Lambda
 
 // Sample DELETE request to APIGateway -> deleteResource Lambda
+function deleteResource() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('DELETE', 'https://5tg521dz44.execute-api.us-east-2.amazonaws.com/dev/realgood');
+  xhr.onreadystatechange = function(event) {
+    console.log(event.target.response);
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+}
+
+// Sample GET request to APIGateway -> getResource Lambda
+function getAllResource() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://5tg521dz44.execute-api.us-east-2.amazonaws.com/dev/realgood/all');
+  xhr.onreadystatechange = function(event) {
+    console.log(event.target.response);
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+}
+
+function getResource() {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://5tg521dz44.execute-api.us-east-2.amazonaws.com/dev/realgood/single');
+  xhr.onreadystatechange = function(event) {
+    console.log(event.target.response);
+  }
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.send();
+}
