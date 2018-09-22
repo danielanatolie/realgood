4ebf9738e61b39a0eeb98c788159f229bcb296ea
Daniel Anatolie <daniel.anatolie@gmail.com>
2018-09-22 12:21:07 -0700

    Sample POST request to APIGateway -> postResource Lambda

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,11 @@
 /*
   All of these functions are going to be living in the aws cloud as lambdas
 */
+
+// Sample POST request to APIGateway -> postResource Lambda
+var xhr = new XMLHttpRequest();
+xhr.open('POST', 'https://5tg521dz44.execute-api.us-east-2.amazonaws.com/dev/realgood');
+xhr.onreadystatechange = function(event) {
+  console.log(event.target.response);
+}
+xhr.send();
